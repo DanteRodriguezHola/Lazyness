3f7700b987151a9d816541ddc36164162875cb10
DanteRodriguezHola <rodriguezmedrano@alumno.huergo.edu.ar>
2026-04-09 15:33:39 -0300

    Layout más completo de los controles

--- en_reproduccion.py
+++ en_reproduccion.py
@@ -103,33 +103,57 @@ class EnReproduccion:
         label_caratula.grid(column = 1, row = 1)
 
         titulo = StringVar(value = metadatos.title)
-        label_titulo = ttk.Label(frame_metadatos, textvariable = titulo)
-        label_titulo["font"] = "TkHeadingFont:"
-        label_titulo.grid(column = 1, row = 2)
+        self.label_titulo = ttk.Label(frame_metadatos, textvariable = titulo)
+        self.label_titulo["font"] = "TkHeadingFont:"
+        self.label_titulo.grid(column = 1, row = 2)
 
         artista = StringVar(value = metadatos.artist)
-        label_artista = ttk.Label(frame_metadatos, textvariable = artista)
-        label_artista.grid(column = 1, row = 3)
+        self.label_artista = ttk.Label(frame_metadatos, textvariable = artista)
+        self.label_artista.grid(column = 1, row = 3)
 
         album = StringVar(value = metadatos.album)
-        label_album = ttk.Label(frame_metadatos, textvariable = album)
-        label_album.grid(column = 1, row = 4)
+        self.label_album = ttk.Label(frame_metadatos, textvariable = album)
+        self.label_album.grid(column = 1, row = 4)
 
         # === FRAME DE LOS CONTROLES ===
 
         frame_controles = ttk.Frame(mainframe, width = 800, height = 250)
+        frame_controles["padding"] = 5
         frame_controles["relief"] = "sunken"
         frame_controles.grid(column = 1, row = 2)
 
-        boton_anterior = ttk.Button(frame_controles, text = "<I")
+            # === CREACIÓN DE LOS CONTROLES ===
+
+        control_posicion = ttk.Scale(frame_controles, length = 750, from_= 0, to = 100)
+        control_posicion.grid(column = 1, columnspan = 7, row = 1)
+
+        boton_anterior = ttk.Button(frame_controles, text = "◀|")
         boton_anterior["command"] = trafico.anterior_cancion
         boton_anterior.grid(column = 1, row = 3)
 
-        boton_siguente = ttk.Button(frame_controles, text = "I>")
+        boton_retroceder = ttk.Button(frame_controles, text = "◀◀")
+        boton_retroceder.grid(column = 2, row = 3)
+
+        estado = StringVar(value = "")
+        boton_estado = ttk.Button(frame_controles, textvariable = estado)
+        boton_estado.grid(column = 3, row = 3)
+
+        boton_stop = ttk.Button(frame_controles, text = "■")
+        boton_stop.grid(column = 4, row = 3)
+
+        boton_retroceder = ttk.Button(frame_controles, text = "▶▶")
+        boton_retroceder.grid(column = 5, row = 3)
+
+        boton_siguente = ttk.Button(frame_controles, text = "|▶")
         boton_siguente["command"] = trafico.siguente_cancion
-        boton_siguente.grid(column = 2, row = 3)
+        boton_siguente.grid(column = 6, row = 3)
 
+        control_volumen = ttk.Scale(frame_controles, length = 150, from_ = 0, to = 9)
+        control_volumen.grid(column = 7, row = 3)
 
+            # === CREACIÓN DE LOS CONTROLES ===
+
+        # === FRAME DE LOS BOTONES ===
 
         root.mainloop()
 
@@ -141,7 +165,7 @@ class EnReproduccion:
         return ruta_caratula
     
     def actualizar_metadatos(self):
-        pass
+        self.label_titulo.set()
 
 
 ahora = EnReproduccion()
