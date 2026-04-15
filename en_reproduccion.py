80126c3e48e0fcfa6c0a6a166ca77e7db6994f92
DanteRodriguezHola <rodriguezmedrano@alumno.huergo.edu.ar>
2026-04-15 10:48:19 -0300

    Ahora el boton de stop funciona

--- en_reproduccion.py
+++ en_reproduccion.py
@@ -7,7 +7,6 @@ from abrir_carpeta import abrir_carpeta
 import reproductor as r
 from reproductor import mixer
 
-
 root = Tk()
 root.title("Lazyness")
 root.geometry("800x600")
@@ -17,21 +16,38 @@ mainframe = ttk.Frame(root, width = 800, height = 600)
 mainframe["padding"] = 10
 mainframe.grid(column = 1, row = 1)
 
-def cambiar_estado():
+def actualizar_metadatos(cancion):
+    metadatos = TinyTag(cancion)
+
+def reproducir_cancion(cancion):
+    mixer.music.load(cancion)
+    mixer.music.play()
+
+    if mixer.music.get_busy() or r.estado == r.PAUSA:
+        actualizar_metadatos(cancion)
+
+def cambiar_estado(boton):
     if r.estado == r.REPRODUCCION:
         mixer.music.pause()
+        boton.config(text = "▶")
 
     elif r.estado == r.PAUSA:
         mixer.music.unpause()
+        boton.config(text = "I I")
 
     r.estado = not(r.estado)
 
+def detener_cancion(boton):
+    mixer.music.set_pos(0.00)
+    r.estado == r.PAUSA
+
+    cambiar_estado(boton)
+
 # === FRAME DE LA CARÁTULA ===
 
-cancion = "C:/Users/Public/Music/2026.01.15 - Eyeball/01 - Eyeball.flac"
+cancion = "C:/Users/Public/Music/2026.01.15 - Eyeball/02 - The Glamour of Rock.flac"
 
-mixer.music.load(cancion)
-mixer.music.play()
+
 
 metadatos = TinyTag.get(cancion)
 
@@ -62,7 +78,7 @@ artista = StringVar(value = "Artista desconocido")
 label_artista = ttk.Label(frame_detalles, textvariable = artista)
 label_artista.grid(column = 1, row = 3)
 
-album = StringVar(value = "Album desconocido")
+album = StringVar(value = "Álbum desconocido")
 label_album = ttk.Label(frame_detalles, textvariable = album)
 label_album.grid(column = 1, row = 4)
 
@@ -86,12 +102,12 @@ boton_anterior.grid(column = 1, row = 3)
 boton_retroceder = ttk.Button(frame_controles, text = "◀◀", width = 2)
 boton_retroceder.grid(column = 2, row = 3)
 
-estado = StringVar(value = "I I")
-boton_estado = ttk.Button(frame_controles, textvariable = estado, width = 2)
-boton_estado["command"] = cambiar_estado
+boton_estado = ttk.Button(frame_controles, text = "I I", width = 2)
+boton_estado["command"] = lambda boton = boton_estado: cambiar_estado(boton)
 boton_estado.grid(column = 3, row = 3)
 
 boton_stop = ttk.Button(frame_controles, text = "■", width = 2)
+boton_stop["command"] = lambda boton = boton_estado: detener_cancion(boton)
 boton_stop.grid(column = 4, row = 3)
 
 boton_retroceder = ttk.Button(frame_controles, text = "▶▶", width = 2)
@@ -101,16 +117,16 @@ boton_siguente = ttk.Button(frame_controles, text = "|▶", width = 2)
 boton_siguente.grid(column = 6, row = 3)
 
 control_volumen = ttk.Scale(frame_controles, length = 150, from_ = 0, to = 9)
-control_volumen.grid(column = 7, row = 3)
+control_volumen.grid(column = 7, row = 4)
 
     # === CREACIÓN DE LOS CONTROLES ===
 
-boton_abrir_carpeta = ttk.Button(frame_controles, text = "+", width = 2)
+boton_abrir_carpeta = ttk.Button(frame_controles, text = "Abrir carpeta")
 boton_abrir_carpeta["command"] = abrir_carpeta
-boton_abrir_carpeta.grid(column = 1, row = 4)
+boton_abrir_carpeta.grid(column = 1, row = 5)
 
 # === FRAME DE LOS BOTONES ===
 
-
+reproducir_cancion(cancion)
 
 root.mainloop()
